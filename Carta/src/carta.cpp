6f030d642cb9c71d6ff9067020c50e4ce6809524
Dennis <31170537+llecram@users.noreply.github.com>
2018-10-08 14:16:45 -0500

    Update carta.cpp

--- Carta/src/carta.cpp
+++ Carta/src/carta.cpp
@@ -1,2 +1,14 @@
 #include "carta.h"
 carta::carta(int a,char b):numero(a),palo(b){};
+void carta::Print(){
+	if (numero == 1)
+		cout << "| A " << palo << " |" << endl;
+	else if (numero == 11)
+		cout << "| J " << palo << " |" << endl;
+	else if (numero == 12)
+		cout << "| Q " << palo << " |" << endl;
+	else if (numero == 13)
+		cout << "| K " << palo << " |" << endl;
+	else
+		cout << "| " << numero << " " << palo << " |" << endl;
+}
